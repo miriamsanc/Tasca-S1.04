0f6bcdc3bca64545196b9c43b8b20f8f61e7586d
Miriam <miriamsd94@gmail.com>
2026-04-18 10:47:05 +0200

    Refactored a little bit

--- Level 1/Ex2.php
+++ Level 1/Ex2.php
@@ -3,24 +3,24 @@
 Escriu un programa que defineixi una classe Shape amb un constructor que rebi com a paràmetres l'ample i alt. Defineix dues subclasses; Triangle i Rectangle que heretin de Shape i que calculin respectivament l'àrea amb un mètode.
 */
 class Shape {
-    public $width;
-    public $height;
+    public float $width;
+    public float $height;
 
-    public function __construct($width, $height) {
+    public function __construct(float $width, float $height) {
         $this->width = $width;
         $this->height = $height;
     }
 }
 
 class Triangle extends Shape {
-    public function calculateArea(){
+    public function calculateArea(): float {
         return $this->width*$this->height*0.5;
     }
 
 }
 
 class Rectangle extends Shape {
-    public function calculateArea(){
+    public function calculateArea(): float {
         return $this->width*$this->height;
     }
 
@@ -32,7 +32,5 @@ echo "Area del triangulo: " . $triangle->calculateArea() . "<br>";
 
 $rectangle= new Rectangle(15,4);
 echo "Area del rectangulo: " . $rectangle->calculateArea() . "<br>";
-
-
 
 ?>
